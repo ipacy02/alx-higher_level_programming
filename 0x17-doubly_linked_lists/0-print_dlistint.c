fa19ed3cc428374cee637f9efb8a5a9ba26c522e
ipacy02 <ipacy02@gmail.com>
2024-01-12 14:20:40 -0800

    hjasdg

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,31 @@
-#!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    y = 0
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end='')
-            y += 1
-        except IndexError:
-            break
-    print()
-    return y
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
+
